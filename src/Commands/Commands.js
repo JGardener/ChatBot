3203879e9c17b1d1f2b610866ef1eb5e71f554fa
JGardener <jamesgardener92@gmail.com>
2021-12-13 10:09:02 +0000

    Followage initial commit. See comments

--- src/Commands/Commands.js
+++ src/Commands/Commands.js
@@ -1,9 +1,11 @@
 import Dice from './Dice';
 import Lurk from './Lurk';
 import Hug from './Hug';
+import Followage from './Followage'
 
 let Commands = {
     "!dice": Dice,
+    "!followage": Followage,
     "!lurk": Lurk,
     "!hug": Hug
 }
---
+++ src/Commands/Followage.js
@@ -0,0 +1,43 @@
+
+import fetch from 'node-fetch';
+import { DateTime } from 'luxon';
+
+const Followage = (chatParams) => {
+    
+    const getUserFollowAge = (userId) => {
+        fetch(`https://api.twitch.tv/helix/users/follows?from_id=${userId}&to_id=${process.env.CHANNEL_ID}`, {
+            'method': 'GET',
+            headers: {
+                'Client-ID': process.env.BOT_CLIENT_ID,
+                'Authorization': 'Bearer ' + process.env.BOT_ACCESS_TOKEN,
+            }
+        })
+        .then((response) => response.json())
+        .then((twitch) => {
+
+            let followStart = DateTime.fromISO(twitch.data[0].followed_at);
+            let rightNow = DateTime.now();
+            let difference = rightNow.diff(followStart, ['years', 'months', 'weeks', 'days', 'hours', 'minutes', 'seconds']).toObject();
+            let differenceArray = [];
+            
+            console.log('Line 23:', Object.entries(difference));
+
+            // Converts the diff object to a string.
+            let differenceMessage = () => {
+                for (const [key, value] of Object.entries(difference)){
+                    if(value != 0){
+                        differenceArray.push(`${value} ${key}`);
+                    }
+                }
+                return `${differenceArray.join(', ')} ago`;
+            }
+            
+            chatParams.client.say(chatParams.channel, 
+                `${chatParams.tags['display-name']} has followed Rom since ${followStart.toFormat('DDDD')}, ${differenceMessage()}`);
+        });    
+    }
+    getUserFollowAge(chatParams.tags['user-id']);
+}
+
+export default Followage;
+
