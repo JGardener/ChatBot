0ca0eb9e541bea3cd3d766b1b7efb7b0d1ccf6ba
JGardener <jamesgardener92@gmail.com>
2021-12-13 17:01:37 +0000

    Token refresh. I realised I needed to refresh my access token for authorisation in this command, updated with a separate command to do this. Need to test on a live broadcast

--- src/Commands/Clip.js
+++ src/Commands/Clip.js
@@ -3,19 +3,41 @@ import { DateTime } from "luxon";
 
 const API_URL = `https://api.twitch.tv/helix/clips?broadcaster_id=${process.env.CHANNEL_ID}'`
 
-const Clip = (chatParams) => {
-    const createClip =  () => {fetch(API_URL, {
-        method: "POST",
-        headers: {
-            'Client-ID': process.env.BOT_CLIENT_ID,
-            'Authorization': 'Bearer ' + process.env.BOT_ACCESS_TOKEN,
-        }
-    }).then(response => response.json()).then(twitch => {
-           console.log(twitch);
-        });
+// First, we need an access token to get authorisation. 
+const refreshToken = async () => {
+    let accessToken = ""
+    const response = await fetch(`https://id.twitch.tv/oauth2/token
+        --data-urlencode
+        ?grant_type=refresh_token
+        &refresh_token=${process.env.REFRESH_TOKEN}
+        &client_id=${process.env.CLIENT_ID}
+        &client_secret=${process.env.CLIENT_SECRET}`, {
+        method: "POST"
+    });
+    const data = await response.json();
+    accessToken = `Bearer ${data["access_token"]}`;
+    return accessToken;
     }
+    
+
+
+const createClip =  (chatParams, accessToken) => {fetch(API_URL, {
+    method: "POST",
+    headers: {
+        'Client-ID': process.env.BOT_CLIENT_ID,
+        'Authorization': 'Bearer ' + accessToken,
+    }
+}).then(response => response.json()).then(twitch => {
+        console.log(twitch);
+    });
 }
-Clip();
+
+
+const Clip = (chatParams) => {
+    refreshToken().then((accessToken) => {
+        createClip(chatParams, accessToken)
+    })
+}
 
 export default Clip;
 
