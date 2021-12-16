224cb71604c0311b5caaa705500403ba1902eb11
JGardener <jamesgardener92@gmail.com>
2021-12-16 16:00:09 +0000

    Clip update. Adjusted fetch parameters for correct authorisation to Twitch API. Converted createClip to async/await for more efficient code flow. Added a Discord webhook for prosperity of Twitch clips outside of the clip page on Twitch.

--- src/Commands/Clip.js
+++ src/Commands/Clip.js
@@ -1,43 +1,42 @@
 import fetch from "node-fetch";
 import { DateTime } from "luxon";
 
-const API_URL = `https://api.twitch.tv/helix/clips?broadcaster_id=${process.env.CHANNEL_ID}'`
-
-// First, we need an access token to get authorisation. 
-const refreshToken = async () => {
-    let accessToken = ""
-    const response = await fetch(`https://id.twitch.tv/oauth2/token
-        --data-urlencode
-        ?grant_type=refresh_token
-        &refresh_token=${process.env.REFRESH_TOKEN}
-        &client_id=${process.env.CLIENT_ID}
-        &client_secret=${process.env.CLIENT_SECRET}`, {
-        method: "POST"
-    });
-    const data = await response.json();
-    accessToken = `Bearer ${data["access_token"]}`;
-    return accessToken;
-    }
-    
-
-
-const createClip =  (chatParams, accessToken) => {fetch(API_URL, {
+const createClip = async (chatParams) => {
+    const response = await fetch(`https://api.twitch.tv/helix/clips?broadcaster_id=${process.env.CHANNEL_ID}`, {
     method: "POST",
     headers: {
-        'Client-ID': process.env.BOT_CLIENT_ID,
-        'Authorization': 'Bearer ' + accessToken,
+    'Client-ID': process.env.BOT_CLIENT_ID,
+    'Authorization': 'Bearer ' + process.env.BOT_ACCESS_TOKEN,
     }
-}).then(response => response.json()).then(twitch => {
-        console.log(twitch);
     });
-}
+    const twitch = await response.json();
+    console.log(twitch);
+    chatParams.client.say(chatParams.channel, `${chatParams.tags['display-name']} has clipped successfully! You will find the clip here: http://clips.twitch.tv/${twitch.data[0].id}`)
+    const data = {
+        displayName: chatParams.tags['display-name'],
+        editURL: `http://clips.twitch.tv/${twitch.data[0].id}`
+    };
+    return data;
+};
 
+const sendToDiscord = (displayName, editURL) => {
+    return fetch(process.env.DISCORD_WEBHOOK, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, 
+        body: JSON.stringify({
+            username: process.env.DISCORD_USERNAME,
+            content: `${displayName} clipped the stream at ${DateTime.now().toLocaleString(DateTime.TIME_SIMPLE)}! ${editURL}`
+        })
+    });
+};
 
 const Clip = (chatParams) => {
-    refreshToken().then((accessToken) => {
-        createClip(chatParams, accessToken)
-    })
-}
+    createClip(chatParams)
+        .then(object => {
+            sendToDiscord(object.displayName, object.editURL)
+        })
+};
 
 export default Clip;
-
